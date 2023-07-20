d3da3c11d7cd8c10b1fe28fd01d5212a2e60bc3e
Lucas Micheleto <lucasnovo@outlook.com>
2023-07-20 00:44:58 -0300

    add product service on client

---
+++ Client/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Services
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+
+        Task GetProductsAsync();
+    }
+}
---
+++ Client/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorEcommerce.Shared;
+
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public List<Product> Products { get; set; }
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task GetProductsAsync()
+        {
+            var res = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("/api/Product");
+            if (res != null && res.Success && res.Data != null)
+                Products = res.Data;
+        }
+    }
+}
